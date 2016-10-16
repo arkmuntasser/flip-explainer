76c21ca2b1048291ccb8955e4e624a06c187d9cc
Amir R Muntasser <arkmuntasser@gmail.com>
2016-10-16 09:01:44 -0700

    added closing animation to good example, styling

--- good-example/expand.js
+++ good-example/expand.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 
   root.find('[data-list-item]').on('click', function() {
     if($(this).hasClass('open')) {
-      root.find('[data-list-item]').removeClass('open animatable');
+      unflip(root.find('[data-list-item].open'));
     } else {
-      root.find('[data-list-item]').removeClass('open animatable');
+      unflip(root.find('[data-list-item].open'));
 
       flip($(this));
     }
@@ -17,9 +17,9 @@ $(document).ready(function() {
 
     // Last
     node.addClass('open');
+    var last = node[0].getBoundingClientRect();
 
     // Invert
-    var last = node[0].getBoundingClientRect();
     var invertWidth = first.width / last.width;
     var invertHeight = first.height / last.height;
 
@@ -31,4 +31,35 @@ $(document).ready(function() {
       node[0].style.transform = '';
     });
   }
+
+  var unflip = function(node) {
+    if(node.length !== 0) {
+      // First
+      var first = node[0].getBoundingClientRect();
+
+      // Last
+      node.removeClass('open animatable').addClass('closing');
+      var last = node[0].getBoundingClientRect();
+
+      // Invert
+      var invertWidth =  first.width / last.width;
+      var invertHeight = first.height / last.height;
+
+      node[0].style.transform = 'scale(' + invertWidth + ',' + invertHeight + ')';
+
+      requestAnimationFrame(function() {
+        // Play
+        node.addClass('closing-animatable');
+        node[0].style.transform = '';
+      });
+    }
+  }
+
+  // Cleanup when the transitions from unflip() are over.
+  // Limit to 1 listener on the parent triggered by event coming from child
+  root.on('transitionend', '[data-list-item]', function() {
+    if($(this).hasClass('closing')) {
+      $(this).removeClass('closing closing-animatable');
+    }
+  });
 });
